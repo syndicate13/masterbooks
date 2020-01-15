cbef2612d08d4468fe87171ea032bd43314a1a2e
Igor Snisarenko <ihorsnisarenko@gmail.com>
2020-01-15 23:11:55 +0300

    Главная страница

---
+++ js/map_4.js
@@ -0,0 +1,34 @@
+ymaps.ready(function () {
+
+    var myMap = new ymaps.Map('map4', {
+            center: [55.798100, 37.586149],
+            zoom: 16
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+
+        // Создаём макет содержимого.
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        ),
+
+        myPlacemark1 = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'Собственный значок метки',
+            balloonContent: 'Это красивая метка'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: 'img/svg/marker.svg',
+            // Размеры метки.
+            iconImageSize: [35, 50],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-17, -25]
+        });
+
+    myMap.geoObjects
+        .add(myPlacemark1);
+
+});
